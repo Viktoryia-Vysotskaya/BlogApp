51ea93ca7ad0e2851de9338c3973d2934bf1ec48
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-07-07 11:35:09 +0200

    Post editing

--- src/components/pages/EditPost.js
+++ src/components/pages/EditPost.js
@@ -1,7 +1,16 @@
+import { Row, Col } from "react-bootstrap";
+import EditPostForm from "../features/EditPostForm";
+
 const EditPost = () => {
     return (
-        <h2> Edit Post </h2>
-    );
+        <>
+            <Row className="justify-content-center">
+                <Col xs="12" md="10" lg="8">
+                    <h1 className="mb-5"> Edit post </h1>
+                    <EditPostForm />
+                </Col>
+            </Row>
+        </>);
 };
 
 export default EditPost;
